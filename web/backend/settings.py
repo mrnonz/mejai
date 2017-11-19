585b09b64bc820213cf0a63a5eb49eb859faeefe
Nontawat Numor <nontawat39@gmail.com>
2017-11-19 15:39:49 +0700

    Add login template :tea:

--- web/backend/settings.py
+++ web/backend/settings.py
@@ -54,7 +54,9 @@ ROOT_URLCONF = 'backend.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            os.path.join(BASE_DIR, 'templates')
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -121,3 +123,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+
+LOGIN_REDIRECT_URL = '/'
