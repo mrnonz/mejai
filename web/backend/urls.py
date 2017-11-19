fa05afbf26875e7561fe308bd1555a071660ca36
Nontawat Numor <nontawat39@gmail.com>
2017-11-19 15:25:30 +0700

    Remove auth :tea:

--- web/backend/urls.py
+++ web/backend/urls.py
@@ -15,7 +15,6 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
-from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
