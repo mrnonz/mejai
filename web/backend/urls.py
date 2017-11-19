5120e5e77c92c388f28bc94aa4e14f3dbf0ce680
Nontawat Numor <nontawat39@gmail.com>
2017-11-19 15:25:18 +0700

    Clean code :tea:

--- web/backend/urls.py
+++ web/backend/urls.py
@@ -18,7 +18,5 @@ from django.contrib import admin
 from django.contrib.auth import views as auth_views
 
 urlpatterns = [
-    url(r'^login/$', auth_views.login, name='login'),
-    url(r'^logout/$', auth_views.logout, name='logout'),
     url(r'^admin/', admin.site.urls),
 ]
