40e6018b66dbc8164d4689be6229f0a8bdf1c236
HanaM1ni <immicky.a@gmail.com>
2017-11-19 20:04:13 +0700

    Initial Front

---
+++ client/next.config.js
@@ -0,0 +1,26 @@
+var path = require('path');
+
+module.exports = {
+  webpack: (config, { dev }) => {
+    // Perform customizations to webpack config
+    config.module.rules.push({
+        test: /\.css$/,
+        use: [ 'style-loader', 'css-loader' ]
+      })
+      config.module.loaders = [{
+        test: /\.(jpe?g|png|gif|svg)$/i,
+        loaders: [
+            'file-loader?hash=sha512&digest=hex&name=[hash].[ext]',
+            'image-webpack-loader?bypassOnDebug&optimizationLevel=7&interlaced=false'
+        ]
+      }]
+    // Important: return the modified config
+    return config
+  },
+  webpackDevMiddleware: config => {
+    // Perform customizations to webpack dev middleware config
+
+    // Important: return the modified config
+    return config
+  }
+}
---
+++ client/pages/index.js
@@ -0,0 +1,107 @@
+import React, { Component } from 'react'
+import Head from 'next/head'
+import { Form, Container, Grid } from 'semantic-ui-react'
+import styled from 'styled-components'
+
+const PageWrapper = styled.div`
+  background-image: url('/static/loginBG.jpg');
+  background-repeat: no-repeat;
+  background-size: cover;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+`
+
+const FormWrapper = styled.div`
+  background: rgba(135,135,135,0.8);
+  padding: 50px 30px;
+`
+const LogoWrapper = styled.div` 
+  display: flex;
+  justify-content: center;
+  margin-bottom: 30px;
+  .web-logo {
+    font-family: 'Kanit', sans-serif;
+    font-size: 62px;
+  }
+
+  .left-logo {
+    color: #316CB0;
+  }
+
+  .right-logo {
+    color: #F4B333;
+  }
+`
+
+const InputBox = styled.input`
+  padding-left: 15px !important;
+  box-sizing: border-box !important;
+  font-size: 18px !important;
+  background-color: rgba(0,0,0,0) !important;
+  border-style: solid !important;
+  border-color: rgb(175, 175, 175) !important;
+  border-width: 0 0 1.5px 0 !important;
+  color: white !important;
+  outline: 0 !important;
+`
+
+class FormExampleCaptureValues extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { name: '', email: '', submittedName: '', submittedEmail: '' }
+  }
+
+  handleChange = (e, { name, value }) => this.setState({ [name]: value })
+
+  handleSubmit = () => {
+    const { name, email } = this.state
+
+    this.setState({ submittedName: name, submittedEmail: email })
+  }
+
+  render() {
+    const { name, email, submittedName, submittedEmail } = this.state
+
+    return (
+      <PageWrapper>
+        <Head>
+            <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.2.2/semantic.min.css"></link>
+            <link href="https://fonts.googleapis.com/css?family=Kanit" rel="stylesheet"></link>
+        </Head>
+        <FormWrapper>
+          <LogoWrapper>
+            <span className="left-logo web-logo">Mejai</span><span className="right-logo web-logo">Charity</span>
+          </LogoWrapper>
+          <Form onSubmit={this.handleSubmit}>
+            <Form.Field>
+              <InputBox placeholder='Username' name='username' />
+            </Form.Field>
+            <Form.Group widths="equal">
+              <Form.Field>
+                <InputBox placeholder='Firstname' name='firstname' />
+              </Form.Field>
+              <Form.Field>
+                <InputBox placeholder='Lastname' name='lastname' />
+              </Form.Field>
+            </Form.Group>
+            <Form.Field>
+              <InputBox placeholder='Password' name='password' type='password' />
+            </Form.Field>
+            <Form.Field>
+              <InputBox placeholder='Confirm Password' name='confirmPassword' type='password' />
+            </Form.Field>  
+            <Form.Button content='Register' color="green" />
+          </Form>
+        </FormWrapper>
+      </PageWrapper>
+    )
+  }
+}
+
+export default FormExampleCaptureValues
