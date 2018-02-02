0a0e93fd719e73661e6480e8c480f6eef6a9276b
Nontawat Numor <nontawat39@gmail.com>
2018-02-02 16:07:00 +0700

    Remove old api app :tea:

--- backend/web/api/views.py
+++
@@ -1,6 +0,0 @@
-from rest_framework import generics
-from .models import Product
-
-
-class CreateView(generics.ListCreateAPIView):
-    queryset = Product.objects.all()
--- backend/web/mejai/settings.py
+++ backend/web/mejai/settings.py
@@ -37,9 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'product',
+    'product.apps.SnippetsConfig',
     'rest_framework',
-    'api',
 ]
 
 MIDDLEWARE_CLASSES = [
