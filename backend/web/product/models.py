d95670866d6ce30dca83972026010f7dbce90308
Nontawat Numor <nontawat39@gmail.com>
2018-02-02 17:12:49 +0700

    Clean code format :tea:

--- backend/web/product/models.py
+++ backend/web/product/models.py
@@ -1,10 +1,3 @@
-# This is an auto-generated Django model module.
-# You'll have to do the following manually to clean this up:
-#   * Rearrange models' order
-#   * Make sure each model has one field with primary_key=True
-#   * Make sure each ForeignKey has `on_delete` set to the desired behavior.
-#   * Remove `managed = False` lines if you wish to allow Django to create, modify, and delete the table
-# Feel free to rename the models, but don't rename db_table values or field names.
 from __future__ import unicode_literals
 
 from django.db import models
@@ -17,13 +10,13 @@ class Product(models.Model):
     quantity = models.IntegerField()
     auction = models.IntegerField()
     type = models.IntegerField()
-    user_id = models.IntegerField(db_column='User_id')  # Field name made lowercase.
+    user_id = models.IntegerField(db_column='User_id')
     viewer = models.IntegerField()
     hit = models.IntegerField()
     thumbnail = models.CharField(max_length=1023)
     created_time = models.DateTimeField()
-    organization_id = models.IntegerField(db_column='Organization_id')  # Field name made lowercase.
-    category_id = models.IntegerField(db_column='Category_id')  # Field name made lowercase.
+    organization_id = models.IntegerField(db_column='Organization_id')
+    category_id = models.IntegerField(db_column='Category_id')
 
     class Meta:
         managed = False
@@ -35,7 +28,7 @@ class ProductAttribute(models.Model):
     size = models.CharField(max_length=255)
     price = models.DecimalField(max_digits=10, decimal_places=2)
     quantity = models.IntegerField()
-    product_id = models.IntegerField(db_column='Product_id')  # Field name made lowercase.
+    product_id = models.IntegerField(db_column='Product_id')
 
     class Meta:
         managed = False
@@ -53,7 +46,7 @@ class ProductCategory(models.Model):
 
 class ProductImage(models.Model):
     url = models.CharField(max_length=1023)
-    product_id = models.IntegerField(db_column='Product_id')  # Field name made lowercase.
+    product_id = models.IntegerField(db_column='Product_id')
 
     class Meta:
         managed = False
