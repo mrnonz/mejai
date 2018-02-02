0db8caff5f56e698b2f55999a5caad300ce1a828
Nontawat Numor <nontawat39@gmail.com>
2018-02-02 16:10:10 +0700

    Clean old code for app :tea:

--- backend/web/mejai/settings.py
+++ backend/web/mejai/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'product.apps.SnippetsConfig',
+    'product.apps.ProductConfig',
     'rest_framework',
 ]
 
