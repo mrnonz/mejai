41a6da2ad6c38a7c73429ed62d87bf06f5a342d8
Nontawat Numor <nontawat39@gmail.com>
2018-02-05 21:58:50 +0700

    Migrate database for organization :tea:

--- backend/web/organization/models.py
+++ backend/web/organization/models.py
@@ -1,6 +1,14 @@
-# -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
 from django.db import models
 
-# Create your models here.
+
+class Organization(models.Model):
+    name = models.CharField(max_length=255)
+    detail = models.TextField(blank=True, null=True)
+    time = models.DateTimeField()
+    fund = models.DecimalField(max_digits=10, decimal_places=2)
+
+    class Meta:
+        managed = False
+        db_table = 'Organization'
