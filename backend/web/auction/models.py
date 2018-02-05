5d8e7f8e757917a3b07232a3a9c5caa6962767c6
Nontawat Numor <nontawat39@gmail.com>
2018-02-05 21:22:34 +0700

    Migrate database for auction :tea:

--- backend/web/auction/models.py
+++ backend/web/auction/models.py
@@ -1,6 +1,14 @@
-# -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
 from django.db import models
 
-# Create your models here.
+
+class Auction(models.Model):
+    product_id = models.IntegerField(db_column='Product_id')  # Field name made lowercase.
+    exp_time = models.DateTimeField()
+    lastest_price = models.DecimalField(max_digits=10, decimal_places=2)
+    price_step = models.DecimalField(max_digits=10, decimal_places=2)
+
+    class Meta:
+        managed = False
+        db_table = 'Auction'
