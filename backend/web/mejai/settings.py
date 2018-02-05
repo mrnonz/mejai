da27467a2ff9065f956c62e441e7eb8da4135c7b
Nontawat Numor <nontawat39@gmail.com>
2018-02-05 21:21:20 +0700

    Create new app auction :tea:

--- backend/web/mejai/settings.py
+++ backend/web/mejai/settings.py
@@ -39,7 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'product.apps.ProductConfig',
-    'order.apps.OrderConfig'
+    'order.apps.OrderConfig',
+    'auction.apps.AuctionConfig'
 ]
 
 MIDDLEWARE_CLASSES = [
