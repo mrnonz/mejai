8da5ab9242635f51bc9cb7acca53df6ff2512033
Nontawat Numor <nontawat39@gmail.com>
2018-02-05 22:05:05 +0700

    Create migration for Organization_Bank :tea:

--- backend/web/organization_bank/models.py
+++ backend/web/organization_bank/models.py
@@ -1,6 +1,16 @@
-# -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
 from django.db import models
 
-# Create your models here.
+
+class OrganizationBank(models.Model):
+    name = models.CharField(max_length=255)
+    type = models.CharField(max_length=255)
+    number = models.CharField(max_length=32)
+    branch = models.CharField(max_length=255)
+    organization_id = models.IntegerField(db_column='Organization_id')  # Field name made lowercase.
+    bank_id = models.IntegerField(db_column='Bank_id')  # Field name made lowercase.
+
+    class Meta:
+        managed = False
+        db_table = 'Organization_Bank'
