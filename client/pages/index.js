be3df84ce688fa9fdfb6ed58e442a1eb02b63f57
hanam1ni <immicky.a@gmail.com>
2018-02-11 00:36:36 +0700

    Add Index page

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,117 +1,65 @@
 import React, { Component } from 'react'
-import Head from 'next/head'
-import { Form, Container, Grid, Button } from 'semantic-ui-react'
-import styled from 'styled-components'
-import InputBox from 'molecules/InputBox'
+import ProductCard from 'molecules/ProductCard'
+import Pagination from 'molecules/Pagination'
+import FilterProduct from 'organisms/FilterProduct'
+import { Dropdown, Menu } from 'semantic-ui-react'
+import categories from 'stores/mock/categories.json'
+import products from 'stores/mock/auction_products.json'
 
-const PageWrapper = styled.div`
-  background-image: url('/static/loginBG.jpg');
-  background-repeat: no-repeat;
-  background-size: cover;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  position: absolute;
-  top: 0;
-  left: 0;
-  width: 100%;
-  height: 100%;
-`
+class MainPage extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            productPage: 0
+        }
+    }
 
-const FormWrapper = styled.div`
-  background: rgba(80,80,80,0.55);
-  padding: 50px 30px;
-`
-const LogoWrapper = styled.div` 
-  display: flex;
-  justify-content: center;
-  margin-bottom: 30px;
-  .web-logo {
-    font-family: 'Kanit', sans-serif;
-    font-size: 62px;
-  }
+    handlePageClick = (page) => {
+        this.setState({
+            productPage: page.selected
+        })
+    }
 
-  .left-logo {
-    color: #316CB0;
-  }
-
-  .right-logo {
-    color: #F4B333;
-  }
-`
-
-const FormButton = styled(Button)`
-  height: 50px !important;
-  font-size: 16px !important;
-  border-radius: 0px !important;
-  background: #38D672 !important;
-  color: white !important;
-  position:relative;
-  top:50%; 
-  left:50%;
-  transform: translateX(-50%);
-`
-
-const CustomFormField = styled(Form.Field)`
-  margin-bottom: 25px !important;
-`
-
-const CustomFormGroup = styled(Form.Group)`
-  margin-bottom: 25px !important;
-`
-
-class FormExampleCaptureValues extends Component {
-  constructor(props) {
-    super(props);
-    this.state = { username: '', email: '', submittedName: '', submittedEmail: '' }
-  }
-
-  handleChange = (e) => {
-    console.log(e.target.name, e.target.value)}
-
-  handleSubmit = () => {
-    const { name, email } = this.state
-
-    this.setState({ submittedName: name, submittedEmail: email })
-  }
-
-  render() {
-    const { name, email, submittedName, submittedEmail } = this.state
-
-    return (
-      <PageWrapper>
-        <Head>
-            <link rel="stylesheet" href="/static/dist/semantic.min.css"></link>
-            <link href="https://fonts.googleapis.com/css?family=Kanit" rel="stylesheet"></link>
-        </Head>
-        <FormWrapper>
-          <LogoWrapper>
-            <span className="left-logo web-logo">Mejai</span><span className="right-logo web-logo">Charity</span>
-          </LogoWrapper>
-          <Form onSubmit={this.handleSubmit}>
-            <CustomFormField>
-              <InputBox label="Username" name="username" onChange={this.handleChange} />
-            </CustomFormField>
-            <CustomFormGroup widths="equal">
-              <Form.Field>
-                <InputBox label="Firstname" name="firstname" />
-              </Form.Field>
-              <Form.Field>
-                <InputBox label="Lastname" name="lastname" />
-              </Form.Field>
-            </CustomFormGroup>
-            <CustomFormField>
-              <InputBox label="Password" name="password" type="password" />
-            </CustomFormField>
-            <CustomFormField>
-              <InputBox label="Confirm Password" name="confirmpassword" type="password" />
-            </CustomFormField>
-            <FormButton content='Register' />
-          </Form>
-        </FormWrapper>
-      </PageWrapper>
-    )
-  }
+    render() {
+        return (
+            <div className="main-page">    
+                <header>
+                    <div className="background-mask">
+                        <h1>Auction Page</h1>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas posuere, augue vitae molestie bibendum , consectetur adipiscing elit. Maecenas posuere, augue vitae molestie bibendum</p>
+                    </div>
+                </header>
+                <main>
+                    <aside>
+                      <h3>Featured Auction</h3>
+                      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                    </aside>
+                    <div className="featured-auction">
+                      { products.data.map((item) => (
+                          <div className="product"><ProductCard auction name={item.name} price={item.price} /></div>
+                      ))}
+                    </div>
+                </main>
+                <header>
+                    <div className="background-mask">
+                        <h2>Heading</h2>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas posuere, augue vitae molestie bibendum , consectetur adipiscing elit. Maecenas posuere, augue vitae molestie bibendum</p>
+                    </div>
+                </header>
+                <main>
+                    <aside>
+                      <h3>Featured Selling</h3>
+                      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                    </aside>
+                    <div className="featured-auction">
+                      { products.data.map((item) => (
+                          <div className="product"><ProductCard name={item.name} price={item.price} /></div>
+                      ))}
+                    </div>
+                </main>
+            </div>
+        )
+    }
 }
 
-export default FormExampleCaptureValues
+export default MainPage
