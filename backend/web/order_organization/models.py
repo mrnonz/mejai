f67d61446b4cb6f1e3135824a9f8b47f8704d763
Nontawat Numor <nontawat39@gmail.com>
2018-02-12 18:15:55 +0700

    Add new migration for organization :tea:

--- backend/web/order_organization/models.py
+++ backend/web/order_organization/models.py
@@ -1,6 +1,16 @@
-# -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
 from django.db import models
 
-# Create your models here.
+
+class OrderOrganization(models.Model):
+    id = models.IntegerField(primary_key=True)
+    slip = models.CharField(max_length=255)
+    time = models.DateTimeField()
+    amount = models.DecimalField(max_digits=10, decimal_places=2)
+    order_id = models.IntegerField(db_column='Order_id')  # Field name made lowercase.
+    organization_id = models.IntegerField(db_column='Organization_id')  # Field name made lowercase.
+
+    class Meta:
+        managed = False
+        db_table = 'Order_Organization'
