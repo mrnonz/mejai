e067c6c927bded079d24eb68a85e3d9be11af0d0
HanaM1ni <immicky.a@gmail.com>
2018-02-26 21:33:42 +0700

    Init ConfirmPost

--- client/pages/confirm-post.js
+++ client/pages/confirm-post.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 import { Container, Header } from 'semantic-ui-react'
-
+import HelpingTable from 'molecules/HelpingTable';
 class ConfirmPost extends Component {
     constructor(props) {
         super(props)
@@ -11,6 +11,13 @@ class ConfirmPost extends Component {
             <Container className="confirm-post-page">
                 <Header as='h2' dividing color="orange" >
                     ข้อมูลการจัดส่ง
+                </Header>
+                <Header as='h5' >
+                    โครงการที่ช่วยเหลือ
+                </Header>
+                <HelpingTable />
+                <Header as='h5' >
+                    สินค้าที่ลงขาย
                 </Header>
             </Container>
         )
