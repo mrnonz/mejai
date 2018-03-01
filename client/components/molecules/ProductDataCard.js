a924d7a85b9f6ed37e182afb44c1baa712916257
HanaM1ni <immicky.a@gmail.com>
2018-03-01 17:46:05 +0700

    Finishing ProductConfirmation for auction

--- client/components/molecules/ProductDataCard.js
+++ client/components/molecules/ProductDataCard.js
@@ -2,27 +2,43 @@ import React from 'react'
 import { Header, Grid } from 'semantic-ui-react'
 import Gallery from 'molecules/Gallery'
 
-const ProductDataCard = (props) => (
+const ProductDataCard = ({ auction }) => (
     <Grid className="product-data-card" container>
         <Grid.Column width={8}>
             <Gallery />
         </Grid.Column>
         <Grid.Column width={8}>
-            <div className="product-data">
-                <Header as="h2">เสื้อสีน้ำเงิน</Header>
-                <Header as="h3" color="grey">ผู้ช่วยเหลือ</Header>
-                <p>สมศรี สมชาย</p>
-                <Header as="h3" color="grey">วันที่ลง</Header>
-                <p>23 ตุลาคม 2560 14.30 น.</p>
-                <Header as="h3" color="grey">หมวดหมู่</Header>
-                <p>เครื่องแต่งกาย</p>
-                <div className="detail-table">
-                    <div><Header as="h4" color="grey">ราคา</Header></div>
-                    <div><p>150 บาท</p></div>
-                    <div><Header as="h4" color="grey">จำนวนสินค้า</Header></div>
-                    <div><p>15 ชิ้น</p></div>
-                </div>
-            </div>
+            {
+                auction ? (
+                    <div className="product-data">
+                        <Header as="h2">เสื้อสีน้ำเงิน</Header>
+                        <Header as="h3" color="grey">ผู้ช่วยเหลือ</Header>
+                        <p>สมศรี สมชาย</p>
+                        <Header as="h3" color="grey">วันสิ้นสุด</Header>
+                        <p>23 ตุลาคม 2560 14.30 น.</p>
+                        <Header as="h3" color="grey">หมวดหมู่</Header>
+                        <p>เครื่องแต่งกาย</p>
+                        <Header as="h3" color="grey">ราคาเริ่มต้น</Header>
+                        <p>150 บาท</p>
+                    </div>
+                ) : (
+                    <div className="product-data">
+                        <Header as="h2">เสื้อสีน้ำเงิน</Header>
+                        <Header as="h3" color="grey">ผู้ช่วยเหลือ</Header>
+                        <p>สมศรี สมชาย</p>
+                        <Header as="h3" color="grey">วันที่ลง</Header>
+                        <p>23 ตุลาคม 2560 14.30 น.</p>
+                        <Header as="h3" color="grey">หมวดหมู่</Header>
+                        <p>เครื่องแต่งกาย</p>
+                        <div className="detail-table">
+                            <div><Header as="h4" color="grey">ราคา</Header></div>
+                            <div><p>150 บาท</p></div>
+                            <div><Header as="h4" color="grey">จำนวนสินค้า</Header></div>
+                            <div><p>15 ชิ้น</p></div>
+                        </div>
+                    </div>
+                )
+            }
         </Grid.Column>
     </Grid>
 ) 
