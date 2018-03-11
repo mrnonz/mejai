89c1d60199e0f1c5dca294efcea59a60106fdeb2
Nontawat Numor <nontawat39@gmail.com>
2018-03-11 14:37:10 +0700

    Add user detail page :tea:

--- backend/web/customer/urls.py
+++ backend/web/customer/urls.py
@@ -3,4 +3,5 @@ from customer import views
 
 urlpatterns = [
     url(r'^customer/$', views.customer_list),
+    url(r'^customer/(?P<pk>[0-9]+)/$', views.customer_detail),
 ]
--- backend/web/customer/views.py
+++ backend/web/customer/views.py
@@ -19,3 +19,27 @@ def customer_list(request):
             serializer.save()
             return JsonResponse(serializer.data, status=201)
         return JsonResponse(serializer.errors, status=400)
+
+
+@csrf_exempt
+def customer_detail(request, pk):
+    try:
+        customer = Customer.objects.get(pk=pk)
+    except Customer.DoesNotExist:
+        return HttpResponse(status=404)
+
+    if request.method == 'GET':
+        serializer = CustomerSerializer(customer)
+        return JsonResponse(serializer.data)
+
+    elif request.method == 'PUT':
+        data = JSONParser().parse(request)
+        serializer = CustomerSerializer(customer, data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return JsonResponse(serializer.data)
+        return JsonResponse(serializer.errors, status=400)
+
+    elif request.method == 'DELETE':
+        customer.delete()
+        return HttpResponse(status=204)
