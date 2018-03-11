f1adc4498733be3c19a3bc3bb312f2686a62e73f
Nontawat Numor <nontawat39@gmail.com>
2018-03-11 22:43:20 +0700

    Add migrate for Cart_Product :tea:

--- backend/web/cart_product/models.py
+++ backend/web/cart_product/models.py
@@ -3,4 +3,15 @@ from __future__ import unicode_literals
 
 from django.db import models
 
-# Create your models here.
+
+class CartProduct(models.Model):
+    id = models.IntegerField(primary_key=True)
+    time = models.DateTimeField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    quantity = models.IntegerField()
+    cart_id = models.IntegerField(db_column='Cart_id')
+    product_id = models.IntegerField(db_column='Product_id')
+
+    class Meta:
+        managed = True
+        db_table = 'Cart_Product'
