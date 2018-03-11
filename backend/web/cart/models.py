7f96159ff59aa5c552807ce070d40c1bfd5daa88
Nontawat Numor <nontawat39@gmail.com>
2018-03-11 22:38:53 +0700

    Add model and migration Cart :tea:

--- backend/web/cart/models.py
+++ backend/web/cart/models.py
@@ -3,4 +3,12 @@ from __future__ import unicode_literals
 
 from django.db import models
 
-# Create your models here.
+
+class Cart(models.Model):
+    id = models.IntegerField(primary_key=True)
+    time = models.DateTimeField()
+    customer_id = models.IntegerField(db_column='Customer_id')
+
+    class Meta:
+        managed = True
+        db_table = 'Cart'
