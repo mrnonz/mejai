4cc6eace51cbc4ab48b4e6058f2183838eb0c55c
Nontawat Numor <nontawat39@gmail.com>
2018-03-12 17:48:55 +0700

    Add relationship auction and auction_customer :tea:

--- backend/web/auction_customer/models.py
+++ backend/web/auction_customer/models.py
@@ -1,14 +1,16 @@
 from __future__ import unicode_literals
 
 from django.db import models
+from auction.models import Auction
 
 
 class AuctionCustomer(models.Model):
     id = models.IntegerField(primary_key=True)
     price = models.DecimalField(max_digits=10, decimal_places=2)
     time = models.DateTimeField()
-    auction_id = models.IntegerField(db_column='Auction_id')  # Field name made lowercase.
     customer_id = models.IntegerField(db_column='Customer_id')  # Field name made lowercase.
+    auction = models.ForeignKey(
+        Auction, on_delete=models.CASCADE, related_name='auction', default='')
 
     class Meta:
         managed = True
