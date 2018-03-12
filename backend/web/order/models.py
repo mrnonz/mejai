61415c6985829f70d348e8cf52cf8ed4ee8bfc4f
Nontawat Numor <nontawat39@gmail.com>
2018-03-12 19:03:45 +0700

    Update order with product :tea:

--- backend/web/order/models.py
+++ backend/web/order/models.py
@@ -1,15 +1,17 @@
 from __future__ import unicode_literals
 
 from django.db import models
+from product.models import Product
 
 
 class Order(models.Model):
     time = models.DateTimeField()
     quantity = models.IntegerField()
     price = models.DecimalField(max_digits=10, decimal_places=2)
-    product_id = models.IntegerField(db_column='Product_id')
     buyer_id = models.IntegerField(db_column='Buyer_id')
     status = models.BooleanField(default=False)
+    product = models.ForeignKey(
+        Product, on_delete=models.CASCADE, related_name='order_product', default='')
 
     class Meta:
         managed = True
