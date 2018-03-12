e2e98add857a41898632e81664ec7f6e87a52b8e
Nontawat Numor <nontawat39@gmail.com>
2018-03-12 18:34:59 +0700

    Add more relationship in order_ganization :tea:

--- backend/web/order_organization/models.py
+++ backend/web/order_organization/models.py
@@ -2,6 +2,7 @@ from __future__ import unicode_literals
 
 from django.db import models
 from order.models import Order
+from organization.models import Organization
 
 
 class OrderOrganization(models.Model):
@@ -9,9 +10,10 @@ class OrderOrganization(models.Model):
     slip = models.CharField(max_length=255)
     time = models.DateTimeField()
     amount = models.DecimalField(max_digits=10, decimal_places=2)
-    organization_id = models.IntegerField(db_column='Organization_id')
     order = models.ForeignKey(
         Order, on_delete=models.CASCADE, related_name='order_organization_order', default='')
+    organization = models.ForeignKey(
+        Organization, on_delete=models.CASCADE, related_name='order_organization_organization', default='')
 
     class Meta:
         managed = True
