c3be916aabe8d4461c07d04b8a7dd2b7ce7b7fe2
Nontawat Numor <nontawat39@gmail.com>
2018-03-12 19:10:41 +0700

    Add relationship cart_customer :tea:

--- backend/web/cart/models.py
+++ backend/web/cart/models.py
@@ -2,12 +2,14 @@
 from __future__ import unicode_literals
 
 from django.db import models
+from customer.models import Customer
 
 
 class Cart(models.Model):
     id = models.IntegerField(primary_key=True)
     time = models.DateTimeField()
-    customer_id = models.IntegerField(db_column='Customer_id')
+    customer = models.ForeignKey(
+        Customer, on_delete=models.CASCADE, related_name='cart_customer', default='')
 
     class Meta:
         managed = True
