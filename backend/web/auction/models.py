0e5b1411f2c8fd73f3bbaf64d2f7c1d5c2de88eb
Nontawat Numor <nontawat39@gmail.com>
2018-03-12 18:20:20 +0700

    Add relationship between auction and product :tea:

--- backend/web/auction/models.py
+++ backend/web/auction/models.py
@@ -1,13 +1,20 @@
 from __future__ import unicode_literals
 
 from django.db import models
+from product.models import Product
 
 
 class Auction(models.Model):
-    product_id = models.IntegerField(db_column='Product_id')  # Field name made lowercase.
+
     exp_time = models.DateTimeField()
     lastest_price = models.DecimalField(max_digits=10, decimal_places=2)
     price_step = models.DecimalField(max_digits=10, decimal_places=2)
+    product = models.OneToOneField(
+        Product,
+        related_name='auction_product',
+        on_delete=models.CASCADE,
+        default=''
+    )
 
     class Meta:
         managed = True
