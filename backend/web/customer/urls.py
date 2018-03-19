5d4235eea64ae0369da45f4d07be8e0d9582007d
Nontawat Numor <nontawat39@gmail.com>
2018-03-19 21:31:02 +0700

    Add customer_cart_auction cart :tea:

--- backend/web/customer/urls.py
+++ backend/web/customer/urls.py
@@ -6,4 +6,6 @@ urlpatterns = [
     url(r'^customer/(?P<pk>[0-9]+)/$', views.customer_detail),
     url(r'^customer/(?P<pk>[0-9]+)/orders/$', views.customer_orders),
     url(r'^customer/(?P<pk>[0-9]+)/cart/$', views.customer_cart),
+    url(r'^customer/(?P<pk>[0-9]+)/cart/auction/$',
+        views.customer_cart_auction),
 ]
