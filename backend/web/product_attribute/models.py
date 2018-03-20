4b4fbbe5039e861dd0eeee75c6a22ccb61cda715
Nontawat Numor <nontawat39@gmail.com>
2018-03-20 23:54:54 +0700

    Add name to product_attribute :tea:

--- backend/web/product_attribute/models.py
+++ backend/web/product_attribute/models.py
@@ -6,6 +6,7 @@ from product.models import Product
 
 
 class ProductAttribute(models.Model):
+    name = models.CharField(max_length=255, default='')
     color = models.CharField(max_length=255)
     size = models.CharField(max_length=255)
     price = models.DecimalField(max_digits=10, decimal_places=2)
