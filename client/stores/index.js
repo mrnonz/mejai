18d3087381ade2dd8eb789aa16d91514c3421850
HanaM1ni <immicky.a@gmail.com>
2018-04-04 23:20:38 +0700

    Remove mock reducer

--- client/stores/index.js
+++ client/stores/index.js
@@ -1,24 +1,26 @@
 import { combineReducers, createStore, applyMiddleware } from 'redux'
 import thunk from 'redux-thunk'
 import { composeWithDevTools } from 'redux-devtools-extension'
-import mock from './reducers/mock'
-import repository from './reducers/repo'
 import product from './reducers/product'
+import products from './reducers/products'
 import organization from './reducers/organization'
 import cart from './reducers/cart'
 
 const storeApp = combineReducers({
-    mock,
-    repository,
     product,
+    products,
     organization,
     cart
 })
 
 const intialStore = {
-    mock: '', 
-    repository: '', 
     product: {
+        isLoading: true,
+        data: {
+            seller: {}
+        }
+    },
+    products: {
         isLoading: true,
         data: []
     },
