64fad501d8fd0b62c48bc768fa5a3f48f4c9bd0a
HanaM1ni <immicky.a@gmail.com>
2018-04-04 23:19:01 +0700

    Remove top bar

--- client/hocs/withTopbar.js
+++ client/hocs/withTopbar.js
@@ -1,6 +1,4 @@
 import React, { Component } from 'react';
-import withRedux from 'next-redux-wrapper'
-import { makeStore } from '../stores'
 import Topbar from 'organisms/Topbar'
 
 const withTopbar = (ComposedComponent) => {
