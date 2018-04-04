72757d1afdb5fa8e2c8c782e9426f0db41917dda
HanaM1ni <immicky.a@gmail.com>
2018-04-04 23:22:04 +0700

    Connect confirm post with posting form

--- client/components/organisms/ProductDetail.js
+++ client/components/organisms/ProductDetail.js
@@ -4,20 +4,24 @@ import Breadcrumb from 'molecules/Breadcrumb'
 import Gallery from 'molecules/Gallery';
 import ProductData from 'molecules/ProductData';
 
-const ProductDetail = (props) => (
+const ProductDetail = ({ itemType, product, onAdd }) => (
     <div className="product-detail">
         <Breadcrumb />
         <div className="product-user">
             <div className="user-info">
-                <span className="role">ผู้ประมูล</span>
-                <span className="username">สมศรี สมชาย</span>
+                <span className="role">ผู้ขาย</span>
+                <span className="username">{ `${product.seller.first_name} ${product.seller.last_name}`}</span>
             </div>
             <div className="user-avatar">
                 <Image src='static/avatar.jpg' size="tiny" avatar/>
             </div>
         </div>
         <Gallery />
-        <ProductData />
+        <ProductData
+            itemType={itemType} 
+            product={product}
+            onAdd={onAdd}
+        />
     </div>
 )
 
--- client/pages/confirm-post.js
+++ client/pages/confirm-post.js
@@ -1,36 +1,68 @@
 import React, { Component } from 'react'
 import { Container, Header, Button } from 'semantic-ui-react'
+import Router from 'next/router'
 import withRedux from 'next-redux-wrapper'
 import { makeStore } from '../stores'
 import withTopbar from 'hocs/withTopbar'
+import Loader from 'molecules/Loader'
 import HelpingTable from 'molecules/HelpingTable'
 import ProductDataCard from 'molecules/ProductDataCard'
+import { fetchProductItem } from 'stores/actions/product'
 
 class ConfirmPost extends Component {
     constructor(props) {
         super(props)
     }
 
+    componentDidMount() {
+        const { url: { query: { id: productId } } } = this.props
+        this.props.fetchProductItem(productId)
+    }
+
+    handleOnSubmit() {
+        Router.push({
+            pathname: '/'
+        })
+    }
+
     render() {
+        const { product: { data: product, isLoading } } = this.props
         return (
             <Container className="confirm-post-page">
                 <Header as='h2' dividing color="orange" >
                     ข้อมูลสินค้า
                 </Header>
-                <Header as='h5' >
-                    โครงการที่ช่วยเหลือ
-                </Header>
-                <HelpingTable />
-                <Header as='h5' >
-                    สินค้าที่ลงขาย
-                </Header>
-                <ProductDataCard />
-                <div className="button-wrapper">
-                    <Button color="teal" size="large" >ดำเนินการต่อ</Button>
-                </div>
+                { isLoading ? <Loader wrapped /> :
+                    <div>
+                        <Header as='h5' >
+                            โครงการที่ช่วยเหลือ
+                        </Header>
+                        <HelpingTable organization={product.organization} />
+                        <Header as='h5' >
+                            สินค้าที่ลงขาย
+                        </Header>
+                        <ProductDataCard product={product} auction={product.type} />
+                        <div className="button-wrapper">
+                            <Button onClick={() => this.handleOnSubmit()} color="teal" size="large" >ดำเนินการต่อ</Button>
+                        </div>
+                    </div>
+                }
             </Container>
         )
     }
 }
 
-export default withRedux(makeStore)(withTopbar(ConfirmPost))
+const mapStateToProps = (state) => ({
+        product: state.product
+    }
+)
+
+const mapDispatchToProps = (dispatch) => {
+    return {
+        fetchProductItem: (productId) => {
+            dispatch(fetchProductItem(productId))
+        }
+    }
+}
+
+export default withRedux(makeStore, mapStateToProps, mapDispatchToProps)(withTopbar(ConfirmPost))
