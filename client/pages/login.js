c44ec230e5b4be728efa2e28857f9f731ed2f516
HanaM1ni <immicky.a@gmail.com>
2018-04-07 01:05:40 +0700

    Update login form

--- client/pages/login.js
+++ client/pages/login.js
@@ -9,37 +9,60 @@ import SiteLogo from 'molecules/SiteLogo'
 class LoginPage extends Component {
   constructor(props) {
     super(props);
-    
+    this.state = {
+      showLoginForm: true
+    }
+  }
+
+  hideLoginForm() {
+    this.setState({
+      showLoginForm: false
+    })
   }
 
   render() {
+    const { showLoginForm } = this.props
     return (
       <div className="login-page">
         <div className="login-form">
           <SiteLogo />
-          <Form>
-            <Form.Field>
-              <Form.Input placeholder='Username' name='username' />
-            </Form.Field>
-            <Form.Group widths="equal">
-              <Form.Field>
-                <Form.Input placeholder='Firstname' name='firstname' />
-              </Form.Field>
-              <Form.Field>
-                <Form.Input placeholder='Lastname' name='lastname' />
-              </Form.Field>
-            </Form.Group>
-            <Form.Field>
-              <Form.Input placeholder='Password' name='password' type='password' />
-            </Form.Field>
-            <Form.Field>
-              <Form.Input placeholder='Confirm Password' name='confirmPassword' type='password' />
-            </Form.Field>
-            <div className="button-group">
-                <Button color="green">Register</Button>
-                <Button basic>Cancel</Button>
-            </div> 
-          </Form>
+            {
+              false ?
+              <Form>
+                <Form.Field>
+                  <Form.Input placeholder='Username' name='username' />
+                </Form.Field>
+                <Form.Group widths="equal">
+                  <Form.Field>
+                    <Form.Input placeholder='Firstname' name='firstname' />
+                  </Form.Field>
+                  <Form.Field>
+                    <Form.Input placeholder='Lastname' name='lastname' />
+                  </Form.Field>
+                </Form.Group>
+                <Form.Field>
+                  <Form.Input placeholder='Password' name='password' type='password' />
+                </Form.Field>
+                <Form.Field>
+                  <Form.Input placeholder='Confirm Password' name='confirmPassword' type='password' />
+                </Form.Field>
+                <div className="button-group">
+                    <Button color="green">Register</Button>
+                    <Button basic>Cancel</Button>
+                </div> 
+              </Form>
+              :
+              <Form>
+                <Form.Input placeholder="อีเมล์" name='email' />
+                <Form.Input placeholder='ชื่อ - สกุล' name='name' />
+                <Form.Input placeholder='รหัสผ่าน' name='password' type='password' />
+                <Form.Input placeholder='ยืนยันรหัสผ่าน' name='confirmPassword' type='password' />
+                <div className="button-group">
+                    <Button color="green">Register</Button>
+                    <Button basic>Cancel</Button>
+                </div> 
+              </Form>
+            }
         </div>
       </div>
     )
