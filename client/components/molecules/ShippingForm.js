e8e066cbccd38f4ec963039466fdab4ed5f61468
HanaM1ni <immicky.a@gmail.com>
2018-04-07 00:06:59 +0700

    Add default form in shipping

--- client/components/molecules/ShippingForm.js
+++ client/components/molecules/ShippingForm.js
@@ -1,17 +1,17 @@
 import React from 'react'
 import { Form } from 'semantic-ui-react'
 
-const ShippingForm = ({ onChange }) => (
+const ShippingForm = ({ onChange, address }) => (
     <Form>
-        <Form.Input name="name" fluid label='ชื่อ - สกุล' placeholder='ชื่อ - สกุล' onChange={onChange} />
-        <Form.Input name="tel" fluid label='เบอร์โทรศัพท์ติดต่อ' placeholder='เบอร์โทรศัพท์ติดต่อ' onChange={onChange} />
+        <Form.Input defaultValue={address.name} name="name" fluid label='ชื่อ - สกุล' placeholder='ชื่อ - สกุล' onChange={onChange} />
+        <Form.Input defaultValue={address.tel} name="tel" fluid label='เบอร์โทรศัพท์ติดต่อ' placeholder='เบอร์โทรศัพท์ติดต่อ' onChange={onChange} />
         <Form.Group widths='equal'>
-            <Form.Input name="district" fluid label='ตำบล / แขวง' placeholder='ตำบล / แขวง' onChange={onChange} />
-            <Form.Input name="subDistrict" fluid label='อำเภอ / เขต' placeholder='อำเภอ / เขต' onChange={onChange} />
+            <Form.Input defaultValue={address.district} name="district" fluid label='ตำบล / แขวง' placeholder='ตำบล / แขวง' onChange={onChange} />
+            <Form.Input defaultValue={address.subDistrict} name="subDistrict" fluid label='อำเภอ / เขต' placeholder='อำเภอ / เขต' onChange={onChange} />
         </Form.Group>
         <Form.Group widths='equal'>
-            <Form.Input name="province" fluid label='จังหวัด' placeholder='จังหวัด' onChange={onChange} />
-            <Form.Input name="postcode" fluid label='รหัสไปรษณีย์' placeholder='รหัสไปรษณีย์' onChange={onChange} />
+            <Form.Input defaultValue={address.province} name="province" fluid label='จังหวัด' placeholder='จังหวัด' onChange={onChange} />
+            <Form.Input defaultValue={address.postcode} name="postcode" fluid label='รหัสไปรษณีย์' placeholder='รหัสไปรษณีย์' onChange={onChange} />
         </Form.Group>
     </Form>
 )
