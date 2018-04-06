a9869584d72822e6c95e9f12a4822c28d7cbcea6
Nontawat Numor <nontawat39@gmail.com>
2018-04-07 01:43:16 +0700

    Add test script for gcs :tea:

---
+++ backend/web/storage.py
@@ -0,0 +1,19 @@
+# Imports the Google Cloud client library
+from google.cloud import storage
+
+# Instantiates a client
+storage_client = storage.Client()
+
+# The name for the new bucket
+bucket_name = 'mejai'
+
+bucket = storage_client.bucket(bucket_name)
+
+print('Bucket {} created.'.format(bucket.name))
+
+blob = bucket.blob('bank/transfer/slip/storage.txt')
+blob.upload_from_filename(filename='./aaa.txt')
+blob.make_public()
+print('Public url is {}.'.format(blob.public_url))
+
+# public_url Returns:	The public URL for this blob
