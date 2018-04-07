3f0c7fb574b0b76920025fa0f9210c5fdd5984f4
HanaM1ni <immicky.a@gmail.com>
2018-04-07 16:50:50 +0700

    Update login form

--- client/pages/login.js
+++ client/pages/login.js
@@ -4,13 +4,13 @@ import { makeStore } from '../stores'
 import withTopbar from 'hocs/withTopbar'
 import { Form, Container, Grid, Button } from 'semantic-ui-react'
 import SiteLogo from 'molecules/SiteLogo'
-
+import FormInput from 'molecules/FormInput'
 
 class LoginPage extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      showLoginForm: true
+      showLoginForm: false
     }
   }
 
@@ -20,46 +20,33 @@ class LoginPage extends Component {
     })
   }
 
+  showLoginForm() {
+    this.setState({
+      showLoginForm: true
+    })
+  }
+
   render() {
-    const { showLoginForm } = this.props
+    const { showLoginForm } = this.state
     return (
       <div className="login-page">
         <div className="login-form">
           <SiteLogo />
             {
-              false ?
+              showLoginForm ?
               <Form>
-                <Form.Field>
-                  <Form.Input placeholder='Username' name='username' />
-                </Form.Field>
-                <Form.Group widths="equal">
-                  <Form.Field>
-                    <Form.Input placeholder='Firstname' name='firstname' />
-                  </Form.Field>
-                  <Form.Field>
-                    <Form.Input placeholder='Lastname' name='lastname' />
-                  </Form.Field>
-                </Form.Group>
-                <Form.Field>
-                  <Form.Input placeholder='Password' name='password' type='password' />
-                </Form.Field>
-                <Form.Field>
-                  <Form.Input placeholder='Confirm Password' name='confirmPassword' type='password' />
-                </Form.Field>
-                <div className="button-group">
-                    <Button color="green">Register</Button>
-                    <Button basic>Cancel</Button>
-                </div> 
+                <FormInput label="อีเมล์" name="email" required />
+                <FormInput label="รหัสผ่าน" name="password" required />
               </Form>
               :
               <Form>
-                <Form.Input placeholder="อีเมล์" name='email' />
-                <Form.Input placeholder='ชื่อ - สกุล' name='name' />
-                <Form.Input placeholder='รหัสผ่าน' name='password' type='password' />
-                <Form.Input placeholder='ยืนยันรหัสผ่าน' name='confirmPassword' type='password' />
+                <FormInput label="อีเมล์" name="email" required />
+                <FormInput label="ชื่อ - สกุล" name="name" required />
+                <FormInput label="รหัสผ่าน" name="password" required />
+                <FormInput label="ยืนยันรหัสผ่าน" name="confirmPassword" required />
                 <div className="button-group">
-                    <Button color="green">Register</Button>
-                    <Button basic>Cancel</Button>
+                  <Button color="green">Register</Button>
+                  <Button basic onClick={this.showLoginForm.bind(this)}>Cancel</Button>
                 </div> 
               </Form>
             }
