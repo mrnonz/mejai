9fafa5757083d00b280d904ef5aa16fb780157ca
HanaM1ni <immicky.a@gmail.com>
2018-04-09 22:28:39 +0700

    Update Order button

--- client/components/molecules/OrderButton.js
+++ client/components/molecules/OrderButton.js
@@ -1,16 +1,16 @@
 import React from 'react'
 import { Button } from 'semantic-ui-react'
 
-const OrderButton = ({ orderStatus }) => {
+const OrderButton = ({ orderStatus, onHandleSlip, onHandleShipping, onHandleReceive }) => {
     switch(orderStatus) {
         case 1:
-            return <Button color="orange" size="large">เพิ่มหลักฐานการโอน</Button>
+            return <Button onClick={onHandleSlip} color="orange" size="large">เพิ่มหลักฐานการโอน</Button>
         case 2:
-            return <Button color="green" size="large">ส่งของแล้ว</Button>
+            return <Button onClick={onHandleShipping} color="green" size="large">ส่งของแล้ว</Button>
         case 3:
-            return <Button color="green" size="large">ได้รับของแล้ว</Button>
+            return <Button onClick={onHandleReceive} color="green" size="large">ได้รับของแล้ว</Button>
         default:
-            return <div></div>
+            return null
     }
 }
 
