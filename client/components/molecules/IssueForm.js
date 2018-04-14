d5951e1bf304ff1c67d2ce9713745749175ea235
HanaM1ni <immicky.a@gmail.com>
2018-04-14 21:20:41 +0700

    Add file limit in issue

--- client/components/molecules/IssueForm.js
+++ client/components/molecules/IssueForm.js
@@ -5,7 +5,7 @@ import UploadForm from 'molecules/UploadForm'
 const IssueForm = (props) => (
     <Form className="issue-form">
         <Form.Input label='หัวข้อ' placeholder='หมายเลขคำสั่งซื้อ เช่น #1445 หรือหัวข้อปัญหาการใช้งาน' />
-        <UploadForm />
+        <UploadForm fileLimit={3} />
         <Form.TextArea label='รายละเอียด' placeholder='รายละเอียดเพิ่มเติมปัญหาที่พบ' />
     </Form>
 )
