e428b9e077e45c7df2ba6401786b55b6a036a80c
HanaM1ni <immicky.a@gmail.com>
2018-04-17 00:41:20 +0700

    Fix breadcrumb category

--- client/components/molecules/Breadcrumb.js
+++ client/components/molecules/Breadcrumb.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react'
 import { Breadcrumb as DefaultBreadcrumb } from 'semantic-ui-react'
+import Categories from 'stores/models/Categories'
 
 const Breadcrumb = ({ product }) => (
     <DefaultBreadcrumb className="breadcrumb">
         <DefaultBreadcrumb.Section>{ product.auction ? 'ประมูลสินค้า' : 'ซื้อสินค้า' }</DefaultBreadcrumb.Section>
         <DefaultBreadcrumb.Divider />
-        <DefaultBreadcrumb.Section>เครื่องแต่งกาย</DefaultBreadcrumb.Section>
+        <DefaultBreadcrumb.Section>{new Categories(product.category_id).categoryName}</DefaultBreadcrumb.Section>
         <DefaultBreadcrumb.Divider />
         <DefaultBreadcrumb.Section active>{ product.name }</DefaultBreadcrumb.Section>
     </DefaultBreadcrumb>
