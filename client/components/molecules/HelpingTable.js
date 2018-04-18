fdef21428bbf3d674c9530cfe3d8f223885f1410
HanaM1ni <immicky.a@gmail.com>
2018-04-18 23:19:44 +0700

    Add hideprice to ItemTable

--- client/components/molecules/HelpingTable.js
+++ client/components/molecules/HelpingTable.js
@@ -1,10 +1,9 @@
 import React, { Component } from 'react'
 import { Table, Image, Progress } from 'semantic-ui-react'
 
-const HelpingTable = ({ organization, hideLabel }) => {
+const HelpingTable = ({ organization, hideLabel, price, hidePrice }) => {
     return (
         <Table basic className="helping-table" fixed>
-            
             <Table.Header>
                 <Table.Row>
                     <Table.HeaderCell width={5} singleLine>{ !hideLabel && "องค์กรที่ช่วยเหลือ" }</Table.HeaderCell>
@@ -16,20 +15,20 @@ const HelpingTable = ({ organization, hideLabel }) => {
             <Table.Body>    
                 <Table.Row>
                     <Table.Cell>
-                        <Image src={'static/OrganizationImage1.jpg'} size="medium" />
+                        <Image src={organization.thumbnail} size="medium" />
                     </Table.Cell>
                     <Table.Cell  className="content">
                         <p className="header">{organization.name}</p>
                         <p className="description">{organization.description}</p>
-                        <p className="helping"><span>จำนวนเงิน: 450 บาท</span> ธ.กสิกรไทย สาขา ลาดกระบัง</p>
-                        <p className="helping">เลขบัญชี: 123-4-56789-0</p>
+                         <p className="helping">{ !hidePrice && <span>จำนวนเงิน: {price} บาท</span>} ธ.กสิกรไทย สาขา ลาดกระบัง</p>
+                        <p className="helping">เลขบัญชี: 123-4-56789-0</p> 
                     </Table.Cell>
                     <Table.Cell className="content" textAlign="right" >
                         <Image src={'static/qr.jpg'} size="small" inline />
                     </Table.Cell>
                     <Table.Cell  verticalAlign="middle" className="content">
                         <p className="description">รายละเอียด PromptPay</p>
-                        <p className="helping"><span>จำนวนเงิน: 450 บาท</span> ธ.กสิกรไทย สาขา ลาดกระบัง</p>
+                        <p className="helping">{ !hidePrice && <span>จำนวนเงิน: {price} บาท</span>} ธ.กสิกรไทย สาขา ลาดกระบัง</p>
                         <p className="helping">เลขบัญชี: 123-4-56789-0</p>
                     </Table.Cell>
                 </Table.Row>
