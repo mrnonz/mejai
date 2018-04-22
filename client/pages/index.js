ea436869a7f1c98eaeee131caa6eb244ccb5a77e
HanaM1ni <immicky.a@gmail.com>
2018-04-22 23:47:32 +0700

    Change some image cover

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react'
 import withRedux from 'next-redux-wrapper'
-import { isNil } from 'lodash'
+import { isNil, sortBy, reverse } from 'lodash'
 import Svg from 'react-inlinesvg'
+import Router from 'next/router'
 import { makeStore } from '../stores'
 import withTopbar from 'hocs/withTopbar'
 import ProductCard from 'molecules/ProductCard'
@@ -15,14 +16,12 @@ import { fetchBuyProducts, fetchAuctionProducts } from 'stores/actions/product'
 class MainPage extends Component {
     constructor(props) {
         super(props);
-        this.state = {
-            productPage: 0
-        }
     }
 
-    handlePageClick = (page) => {
-        this.setState({
-            productPage: page.selected
+    handleCardClick = (productId) => {
+        Router.push({
+            pathname: '/product',
+            query: { id: productId }
         })
     }
 
@@ -32,7 +31,6 @@ class MainPage extends Component {
     }
 
     render() {
-        // TODO Fix Index
         function NextArrow(props) {
             const { className, style, onClick } = props
             return (
@@ -63,22 +61,24 @@ class MainPage extends Component {
 
         const { buying = [], auction = [] } = this.props.products
         const productsLoading = isNil(buying) || isNil(auction)
+        const filterBuying = reverse(sortBy(buying, (item) => item.created_time)).slice(0,10)
+        const filterAuction = reverse(sortBy(auction, (item) => item.created_time)).slice(0,10)
         return (
             <div className="main-page">    
                 <header>
                     <div className="background-mask">
-                        <h1>Auction Page</h1>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas posuere, augue vitae molestie bibendum , consectetur adipiscing elit. Maecenas posuere, augue vitae molestie bibendum</p>
+                        <h1>ยินดีต้อนรับ</h1>
+                        <p>รายได้จากการขายสินค้าจะนำเงินไปช่วยเหลือองค์กรการกุศลที่ผู้ขายกำหนดไว้</p>
                     </div>
                 </header>
                 <main>
                     <aside>
-                      <h3>Featured Auction</h3>
-                      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                      <h3>สินค้าการประมูล</h3>
+                      <p>แสดงผลสินค้าประมูลล่าสุด</p>
                     </aside>
                     <div className="featured-auction">
                         { !productsLoading && <Slider {...settings}>
-                            { auction.map((item) => (
+                            { filterAuction.map((item) => (
                                 <ProductCard 
                                     auction={false} 
                                     auctionData={item.auction}
@@ -86,7 +86,7 @@ class MainPage extends Component {
                                     productId={item.productId} 
                                     name={item.name} 
                                     price={item.price} 
-                                    // onCardClick={onCardClick} 
+                                    onCardClick={this.handleCardClick.bind(this)} 
                                     thumbnail={item.thumbnail}
                                 />
                             )) }
@@ -94,20 +94,20 @@ class MainPage extends Component {
                         }
                     </div>
                 </main>
-                <header>
+                <header className="sub">
                     <div className="background-mask">
-                        <h2>Heading</h2>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas posuere, augue vitae molestie bibendum , consectetur adipiscing elit. Maecenas posuere, augue vitae molestie bibendum</p>
+                        <h2>การช่วยเหลือ</h2>
+                        <p>คุณสามารถเพิ่มสินค้าเข้าระบบและเลือกองค์กรการกุศลที่ต้องการช่วยเหลือได้</p>
                     </div>
                 </header>
                 <main>
                     <aside>
-                      <h3>Featured Selling</h3>
-                      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                      <h3>สินค้าการจำหน่าย</h3>
+                      <p>แสดงผลสินค้าจำหน่ายล่าสุด</p>
                     </aside>
                     <div className="featured-auction">
                         { !productsLoading && <Slider {...settings}>
-                            { buying.map((item) => (
+                            { filterBuying.map((item) => (
                                 <ProductCard 
                                     auction={false} 
                                     auctionData={item.auction}
