57e287198d197c551175b9410d99e6976b8b1362
Nontawat Numor <nontawat39@gmail.com>
2018-04-27 21:57:14 +0700

    Add bank account API list :tea:

--- backend/web/bank/views.py
+++ backend/web/bank/views.py
@@ -1,6 +1,13 @@
-# -*- coding: utf-8 -*-
-from __future__ import unicode_literals
+from django.http import HttpResponse, JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+from rest_framework.parsers import JSONParser
+from bank.models import Bank
+from bank.serializers import BankSerializer
 
-from django.shortcuts import render
 
-# Create your views here.
+@csrf_exempt
+def bank_list(request):
+    if request.method == 'GET':
+        bank = Bank.objects.all()
+        serializer = BankSerializer(bank, many=True)
+        return JsonResponse(serializer.data, safe=False)
